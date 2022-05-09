bf0f24a06bf8c632a89337403d4edda165d8be73
boseul <tool0923@naver.com>
2022-05-09 17:14:52 +0900

    객체 [7-7] ~ [7-15]

--- study01/src/exam04/Stu01.java
+++ study01/src/exam04/Stu01.java
@@ -82,21 +82,26 @@ class SutdaDeck{
 	
 }
 class SutdaCard{
-	int num;
-	boolean isKwang;
+	/*
+	 * [7-14]문제 7-1에 나오는 섯다카드의 숫자와 종류(isKwang)는 사실 한번 값이 지정되면
+	 * 변경되어서는 안 되는 값이다. 카드의 숫자가 한번 잘못 바뀌면 똑같은 카드가 두 장이 될 수도 있기
+	 * 때문이다. 이러한 문제점이 발생하지 않도록 아래의 SutdaCard를 수정하시오.
+	 */
+	final int NUM;
+	final boolean ISKWANG;
 	
 	SutdaCard(){
 		this(1, true);
 	}
 	
 	SutdaCard(int num, boolean isKwang){
-		this.num = num;
-		this.isKwang = isKwang;
+		this.NUM = num;
+		this.ISKWANG = isKwang;
 		
 	}
 // info() 대신 object클래스의 toString()을 오버라이딩 했다. 
 	public String toString() {
-		return num + (isKwang ? "k" : "");
+		return NUM + (ISKWANG ? "k" : "");
 	}
 }
 
---
+++ study01/src/exam04/Stu04.java
@@ -0,0 +1,79 @@
+package exam04;
+
+
+class MyTv2{
+	/*
+	 * [7-10] My Tv2클래스의 멤버 변수 isPowerON, channel, volume을 클래스 외부에서 접근할 수 없도록 
+	 * 제어자를 붙이고 대신 이 멤버변수들의 값을 어디서나 읽고 변경할 수 있도록 getter와 setter 메서드를 추가하라
+	 * 
+	 */
+	
+	private boolean isPowerOn;
+	private int channel;
+	private int volume; 
+	private int prevChannel;	// 이전 채널 값 저장변수
+	
+	final int MAX_VOLUME = 100;
+	final int MIN_VOLUME = 0;
+	final int MAX_CHANNEL = 100;
+	final int MIN_CHANNEL = 1;
+	
+	//(1)
+	public boolean getIsPowerOn() {
+		return this.isPowerOn;
+	}
+	public void setIsPowerOn(boolean isPowerOn) {
+		this.isPowerOn = isPowerOn;
+	}
+	
+	public int getChannel() {
+		return this.channel;
+	}
+	public void setChannel(int channel) {
+		if(channel > MAX_CHANNEL || channel < MIN_CHANNEL) {
+			return;
+		}
+		this.prevChannel = this.channel;	// 현재 채널 저장
+		this.channel = channel;
+	}
+	
+	public int getVolume() {
+		return this.volume;
+	}
+	public void setVolume(int volume) {
+		if(volume > MAX_VOLUME || volume < MIN_VOLUME) {
+			return;
+		}
+		this.volume= volume;
+	}
+	
+	/*
+	 * [7-11] 문제 7-10에서 작성한 MyTv2 클래스에 이전 채널(previous channel)로 이동하는
+	 * 기능의 메서드를 추가해서 실행결과와 같은 결과를 얻도록 하시오.
+	 * 힌트) 이전 채널의 값을 저장할 멤버변수를 정의하라
+	 * 
+	 * 메서드명 : gotoPrevChannel
+	 * 기능 : 현재 채널을 이전 채널로 변경한다.
+	 * 반환타입, 매개변수 : 없음
+	 */
+	
+	public void gotoPrevChannel() {
+		setChannel(prevChannel);
+	}
+	
+	/*
+	 * [7-14] Stu01으로 가면 있음
+	 * [7-15],[7-16] 아이패드에 정리
+	 */
+	
+}
+
+
+public class Stu04 {
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
