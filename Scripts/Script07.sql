7ce4c82772e4d5d08aadcd8ab3ca82674118c3c2
boseul <tool0923@naver.com>
2022-07-06 15:12:39 +0900

    SQL 문제07 : DRUD + SUBQUERY 문제 풀이

--- Scripts/Script07.sql
+++ Scripts/Script07.sql
@@ -80,6 +80,72 @@ WHERE  STU_ID IN (SELECT STU_ID
 				  WHERE STU_TEACHER = '송중기');
 
 
+				 
+/*
+ * 홍유라_문제07
+ * 
+ * 1. score_t 테이블에 데이터를 추가하시오.
+ *   - 학번: 2201, 수학:  70, 영어:  85, 국어: 95, 자바: 90, 과목점수합계: NULL, 과목점수평균: NULL
+ *   - 학번: 2202, 수학: 100, 영어:  95, 국어: 95, 자바: 90, 과목점수합계: NULL, 과목점수평균: NULL
+ *   - 학번: 2203, 수학:  64, 영어:  70, 국어: 76, 자바: 80, 과목점수합계: NULL, 과목점수평균: NULL
+ *   - 학번: 2204, 수학:  97, 영어:  81, 국어: 72, 자바: 88, 과목점수합계: NULL, 과목점수평균: NULL
+ *   - 학번: 2205, 수학:  88, 영어: 100, 국어: 82, 자바: 74, 과목점수합계: NULL, 과목점수평균: NULL
+ * 
+ * 2. UPDATE 문을 사용하여 과목점수 합계를 score_total 에 넣으시오.
+ * 
+ * 3. UPDATE 문을 사용하여 과목점수 평균값을 score_avg 에 넣으시오.
+ *
+ * 4. 서브쿼리를 사용하여 score_t 테이블에서 score_avg 컬럼값을 기준으로 학생들의 등수를 조회하시오.
+ * 	 - '순위', '학번', '과목점수평균' 만 조회되도록 한다.
+ * 	 - 예외적으로 동일한 점수가 있을 시, 순위는 끊이지 않고 순차적으로 이어지게 한다.   
+ *   - 사진과 같이 과목점수평균을 기준으로 내림차순 정렬한다.
+ */
+
+				 
+SELECT * FROM SCORE_T;
+INSERT INTO SCORE_T (STU_ID, MATH, ENG, KOR, JAVA) VALUES(2201, 70,  85,  95, 90);
+INSERT INTO SCORE_T (STU_ID, MATH, ENG, KOR, JAVA) VALUES(2202, 100, 95,  95, 90);
+INSERT INTO SCORE_T (STU_ID, MATH, ENG, KOR, JAVA) VALUES(2203, 64,  70,  76, 80);
+INSERT INTO SCORE_T (STU_ID, MATH, ENG, KOR, JAVA) VALUES(2204, 97,  81,  72, 88);
+INSERT INTO SCORE_T (STU_ID, MATH, ENG, KOR, JAVA) VALUES(2205, 88,  100, 82, 74);
+
+UPDATE SCORE_T 
+   SET SCORE_TOTAL = MATH + ENG + KOR + JAVA 
+ WHERE STU_ID = 2201;
+UPDATE SCORE_T
+   SET SCORE_TOTAL = MATH + ENG + KOR + JAVA 
+ WHERE STU_ID = 2202;
+UPDATE SCORE_T
+   SET SCORE_TOTAL = MATH + ENG + KOR + JAVA
+ WHERE STU_ID = 2203;
+UPDATE SCORE_T
+   SET SCORE_TOTAL = MATH + ENG + KOR + JAVA
+ WHERE STU_ID = 2204;
+UPDATE SCORE_T
+   SET SCORE_TOTAL = MATH + ENG + KOR + JAVA
+ WHERE STU_ID = 2205;
+
+UPDATE SCORE_T 
+   SET SCORE_AVG = (MATH + ENG + KOR + JAVA) / 4
+ WHERE STU_ID = 2201;
+UPDATE SCORE_T 
+   SET SCORE_AVG = (MATH + ENG + KOR + JAVA) / 4
+ WHERE STU_ID = 2202;
+UPDATE SCORE_T 
+   SET SCORE_AVG = (MATH + ENG + KOR + JAVA) / 4
+ WHERE STU_ID = 2203;
+UPDATE SCORE_T 
+   SET SCORE_AVG = (MATH + ENG + KOR + JAVA) / 4
+ WHERE STU_ID = 2204;
+UPDATE SCORE_T 
+   SET SCORE_AVG = (MATH + ENG + KOR + JAVA) / 4
+ WHERE STU_ID = 2205;
+
+-- 점수를 기준으로 ORDER BY 해준다. 
+SELECT RANK() OVER(ORDER BY SCORE_AVG DESC) AS 순위
+	 , STU_ID
+	 , SCORE_AVG
+FROM SCORE_T;
 
 /*
  * 김규연_문제07
@@ -95,3 +161,84 @@ WHERE  STU_ID IN (SELECT STU_ID
  * 				  - 순위는 중복이 되든 안되든 등수가 1씩 증가되도록 하세요.
  * 				  - 순위가 1 ~ 5 위까지만 출력되도록 하세요.
  */
+
+SELECT * FROM JOBS;
+SELECT * FROM bread_t;
+
+-- 이런 경우에는 >= ANY를 사용해야 한다. -> ANY를 사용한 다중행
+SELECT EMPLOYEE_ID
+	 , FIRST_NAME || LAST_NAME NAME
+	 , JOB_ID
+	 , SALARY
+  FROM EMPLOYEES
+ WHERE SALARY >= ANY (SELECT MAX_SALARY
+ 				   	   FROM JOBS
+ 				   	  WHERE JOB_ID LIKE 'SA_%');
+
+SELECT *
+  FROM (SELECT DENSE_RANK () OVER(ORDER BY BREAD_PRICE DESC) 순위
+  			 , BREAD_NAME
+	 		 , BREAD_TOTAL
+	 		 , BREAD_TYPE
+	 		 , BREAD_PRICE
+	     FROM  BREAD_T)
+ WHERE 순위 <= 5 ;
+/*
+ * 김재은_문제07
+ * 
+ * 1. ref_mart_t의 테이블명을 ref_mart_tb로 변경하시오.
+ *    MART_T의 테이블명을 mart_tb로 변경하시오.
+ *    EMP_MART_T의 테이블명을 emp_mart_tb로 변경하시오.
+ * 2. FK_MART_T_BARCODE 제약조건을 제거하시오.
+ *    PK_REF_MART_T_R_BAR 제약조건을 제거하시오.
+ *    ref_mart_tb 테이블을 제거하시오.
+ * 3. emp_mart_tb의 name의 자료형을 가변길이 20자로 변경하시오.
+ * 
+ * 4. mart_tb에 값을 추가시오.
+ *    ('2080', 3000, '애경', '치약' ,'O', 2080, 5)
+ *    ('계란30구', 6000, '농협', '알류', 'O', 9090, 6)
+ * 5. emp_mart_tb에 값을 추가하시오. 
+ *    ('김봉지', 3000, 1), ('최박스', 3200, 2), ('다니엘소스', 3200, 3),
+ *    ('오젤리', 2900, 4), ('이민트', 4000, 5), ('윤아리', 3400 , 6)
+ * 6. 서브쿼리를 이용하여 mart_tb에서 kind의 값이 '젤리'이거나 '과자'가 들어가는 물품을 찾아 
+ *    emp_mart_t 를 조회하시오. 
+ *    (sort와 manage_sort를 이용하세요)
+ */
+
+
+
+ALTER TABLE REF_MART_T RENAME TO ref_mart_tb;
+ALTER TABLE MART_T RENAME TO mart_tb;
+ALTER TABLE EMP_MART_T RENAME TO emp_mart_tb;
+-- 2. 제약조건 & 테이블 삭제
+ALTER TABLE MART_TB DROP CONSTRAINT FK_MART_T_BARCODE ;
+ALTER TABLE MART_TB DROP CONSTRAINT PK_REF_MART_T_R_BAR ;
+DROP TABLE ref_mart_tb;
+
+-- 3. 자료형 길이 변경
+ALTER TABLE EMP_MART_TB MODIFY NAME VARCHAR2(20);
+
+-- 4. MART_TB에 값추가
+INSERT INTO MART_TB VALUES ('2080', 	3000, '애경', '치약', 'O', 2080, 5);
+INSERT INTO MART_TB VALUES ('계란30구', 	6000, '농협', '알류', 'O', 9090, 6);
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'MART_TB';
+
+-- 5. EMP_MART_TB에 값추가
+INSERT INTO emp_mart_tb VALUES('김봉지',		3000, 	1);
+INSERT INTO emp_mart_tb VALUES('최박스', 		3200, 	2);
+INSERT INTO emp_mart_tb VALUES('다니엘소스', 	3200, 	3);
+INSERT INTO emp_mart_tb VALUES('오젤리', 		2900, 	4); 
+INSERT INTO emp_mart_tb VALUES('이민트', 		4000, 	5); 
+INSERT INTO emp_mart_tb VALUES('윤아리', 		3400 , 	6);
+COMMIT;
+
+SELECT * FROM EMP_MART_TB;
+SELECT * FROM MART_TB;
+
+-- 6. 서브쿼리 
+SELECT * 
+  FROM EMP_MART_TB
+ WHERE MANAGE_SORT IN (SELECT SORT
+ 						  FROM MART_TB
+ 						 WHERE KIND = '젤리'
+ 						    OR KIND LIKE '%과자');
