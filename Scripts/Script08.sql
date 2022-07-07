59cf6e0280cedbfee5a5ae9a678d2824ee41835c
boseul <tool0923@naver.com>
2022-07-08 07:19:06 +0900

    SQL 문제8 : 문제 완성

--- Scripts/Script08.sql
+++ Scripts/Script08.sql
@@ -1,6 +1,14 @@
 /*
  * 이보슬_문제08
- *  
+ * 1. '컴잘알'동아리와 '독서왕'동아리에 가입한 학생들의 학번, 이름, 나이, 전화번호, 학년, 반, 동아리명, 동아리 담당 교사 을 출력하시오. 
+ *   		- INNER JOIN을 사용하여 문제를 해결하도록 한다.
+ * 			- STUDENT_TB, LEVEL_TB, CLUB_TB를 사용한다.
+ * 
+ * 2. '컴잘알' 동아리에 가입한 학생 중 가장 나이 많은 학생의 학생명, 나이, 학년, 반, 담당 교사명 을 출력하시오.
+ * 			- JOIN과 서브쿼리를 사용하여 문제를 해결하도록 한다.
+ *			- 테이블 세 개를 조인한다.
+ *			- 가장 나이 많은 학생을 구할 때 이중 서브쿼리가 필요하다. (만약 다른 방법을 해도 정답이 나온다면 다른 방법을 사용해도 무관)
+ * 
  */
 SELECT * FROM LEVEL_TB;
 SELECT * FROM CLUB_TB;
@@ -14,9 +22,7 @@ SELECT * FROM STUDENT_TB;
  * (2) RIGHT OUTER JOIN :  A LEFT OUTTER JOIN B를 기준으로 출력된다.
  * 
  */
--- '컴잘알'동아리와 '독서왕'동아리에 가입한 학생들의 학번, 이름, 나이, 전화번호, 학년, 반, 동아리명, 동아리 담당 교사 을 출력하시오. 
--- INNER JOIN을 사용하여 문제를 해결하도록 한다.
--- STUDENT_TB, LEVEL_TB, CLUB_TB를 사용한다.
+
 SELECT S.STU_ID
 	 , S.STU_NAME
 	 , S.STU_AGE 
@@ -28,17 +34,24 @@ SELECT S.STU_ID
   FROM STUDENT_TB S, LEVEL_TB L, CLUB_TB C 
  WHERE S.CLUB_ID = C.CLUB_ID
    AND S.STU_ID = L.STU_ID;
- -- '컴잘알' 동아리에 가입한 학생 중 가장 나이 많은 학생의 나이, 학년, 반, 담당 교사명 을 출력하시오.
- -- JOIN과 서브쿼리를 사용하여 문제를 해결하도록 한다.
- SELECT MAX(S.STU_AGE) '나이 최대값'
- 	  , L.LEVEL_NO
+
+  
+ SELECT S.STU_NAME 
+ 	  , S.STU_AGE 
+ 	  , L.LEVEL_NO 
  	  , L.CLASS_NO 
- 	  , L.STU_TEACHER
-   JOIN STUDENT_TB S 
-  	 ON S.CLUB_ID  = (SELECT CLUB_ID
-  					  FROM CLUB_TB
-  					 WHERE CLUB_NAME = '컴잘알')
+ 	  , L.STU_TEACHER 
+   FROM STUDENT_TB S 
    JOIN CLUB_TB C
      ON C.CLUB_ID = S.CLUB_ID
    JOIN LEVEL_TB L
-     ON L.
+     ON L.STU_ID = S.STU_ID
+  WHERE S.CLUB_ID = (SELECT CLUB_ID
+  					  FROM CLUB_TB
+  					 WHERE CLUB_NAME = '컴잘알')
+  	AND S.STU_AGE = (SELECT MAX(STU_AGE)
+  	                   FROM STUDENT_TB 
+  	                  WHERE CLUB_ID = (SELECT CLUB_ID
+  					  				     FROM CLUB_TB
+  					 					WHERE CLUB_NAME = '컴잘알'));
+  	
